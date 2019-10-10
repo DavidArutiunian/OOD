18d4eb409facfb2da29bf35d4f1d983d119f1200
David Arutiunian <arutunjan666@mail.com>
2019-10-11 00:03:19 +0300

    feat(factory): more tests for canvas designer

--- factory/src/test/CanvasDesignerTest.kt
+++ factory/src/test/CanvasDesignerTest.kt
@@ -1,16 +1,13 @@
 package test.java
 
-import com.nhaarman.mockitokotlin2.argumentCaptor
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.*
 import main.Point
 import main.designer.CanvasDesigner
-import main.shape.Rectangle
-import main.shape.Shape
-import main.shape.ShapeFactory
+import main.shape.*
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.awt.Color
 import java.util.*
 import kotlin.test.assertEquals
@@ -47,5 +44,160 @@ internal class CanvasDesignerTest {
 
         val actualDescription = captor.firstValue
         assertEquals(description, actualDescription)
+    }
+
+    @Test
+    fun `designer creates draft with red ellipse`() {
+        val captor = argumentCaptor<String>()
+        val description = "ellipse 100 100 50 50 red"
+
+        val ellipse = Ellipse(
+            Point(100.0, 100.0),
+            50.0,
+            50.0,
+            Color.RED
+        )
+
+        val scanner = Scanner("$description\ndraw")
+        val factory = mock<ShapeFactory> {
+            on { createShape(captor.capture()) } doReturn ellipse
+        }
+        val designer = CanvasDesigner(factory)
+
+        val draft = designer.createDraft(scanner)
+
+        val expectedShapeCount = 1
+        assertEquals(expectedShapeCount, draft.getShapeCount())
+        val actualRectangle = draft.getShape(0)
+        assertThat(actualRectangle, `is`(ellipse as Shape))
+
+        val actualDescription = captor.firstValue
+        assertEquals(description, actualDescription)
+    }
+
+    @Test
+    fun `designer creates draft with yellow polygon`() {
+        val captor = argumentCaptor<String>()
+        val description = "polygon 10 10 20 10 20 20 10 20 50 50 40 40 yellow"
+
+        val polygon = RegularPolygon(
+            listOf(
+                Point(10.0, 10.0),
+                Point(20.0, 10.0),
+                Point(20.0, 20.0),
+                Point(10.0, 20.0),
+                Point(50.0, 50.0),
+                Point(40.0, 40.0)
+            ),
+            Color.YELLOW
+        )
+
+        val scanner = Scanner("$description\ndraw")
+        val factory = mock<ShapeFactory> {
+            on { createShape(captor.capture()) } doReturn polygon
+        }
+        val designer = CanvasDesigner(factory)
+
+        val draft = designer.createDraft(scanner)
+
+        val expectedShapeCount = 1
+        assertEquals(expectedShapeCount, draft.getShapeCount())
+        val actualRectangle = draft.getShape(0)
+        assertThat(actualRectangle, `is`(polygon as Shape))
+
+        val actualDescription = captor.firstValue
+        assertEquals(description, actualDescription)
+    }
+
+    @Test
+    fun `designer creates draft with blue triangle`() {
+        val captor = argumentCaptor<String>()
+        val description = "triangle 10 10 10 50 25 50 blue"
+
+        val triangle = Triangle(
+            listOf(
+                Point(10.0, 10.0),
+                Point(10.0, 50.0),
+                Point(25.0, 50.0)
+            ),
+            Color.BLUE
+        )
+
+        val scanner = Scanner("$description\ndraw")
+        val factory = mock<ShapeFactory> {
+            on { createShape(captor.capture()) } doReturn triangle
+        }
+        val designer = CanvasDesigner(factory)
+
+        val draft = designer.createDraft(scanner)
+
+        val expectedShapeCount = 1
+        assertEquals(expectedShapeCount, draft.getShapeCount())
+        val actualRectangle = draft.getShape(0)
+        assertThat(actualRectangle, `is`(triangle as Shape))
+
+        val actualDescription = captor.firstValue
+        assertEquals(description, actualDescription)
+    }
+
+    @Test
+    fun `designer creates multiple figures`() {
+        val captor = argumentCaptor<String>()
+        val description = "triangle 10 10 10 50 25 50 blue\n" +
+            "ellipse 100 100 50 50 red"
+
+        val triangle = Triangle(
+            listOf(
+                Point(10.0, 10.0),
+                Point(10.0, 50.0),
+                Point(25.0, 50.0)
+            ),
+            Color.BLUE
+        )
+
+        val ellipse = Ellipse(
+            Point(100.0, 100.0),
+            50.0,
+            50.0,
+            Color.RED
+        )
+
+        val scanner = Scanner("$description\ndraw")
+        val factory = mock<ShapeFactory> {
+            on { createShape(captor.capture()) } doReturnConsecutively listOf(triangle, ellipse)
+        }
+        val designer = CanvasDesigner(factory)
+
+        val draft = designer.createDraft(scanner)
+
+        val expectedShapeCount = 2
+        assertEquals(expectedShapeCount, draft.getShapeCount())
+        val actualTriangle = draft.getShape(0)
+        assertThat(actualTriangle, `is`(triangle as Shape))
+        val actualEllipse = draft.getShape(1)
+        assertThat(actualEllipse, `is`(ellipse as Shape))
+
+        val actualDescription = captor.allValues
+        assertThat(actualDescription, `is`(description.split("\n")))
+    }
+
+    @Test
+    fun `designer throws if shape not defined`() {
+        val captor = argumentCaptor<String>()
+        val description = "convex 10 10 10 50 25 50 blue\n"
+
+        val message = "Unsupported shape convex"
+
+        val scanner = Scanner("$description\ndraw")
+        val factory = mock<ShapeFactory> {
+            on { createShape(captor.capture()) } doThrow RuntimeException(message)
+        }
+        val designer = CanvasDesigner(factory)
+
+        val exception = assertThrows<RuntimeException> {
+            designer.createDraft(scanner)
+        }
+
+        assertEquals(message, exception.message)
     }
 }
